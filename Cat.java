9b716a5d09e00ae9bdbf310895c2866f2dfd161b
elliecho98 <36868474+elliecho98@users.noreply.github.com>
2018-05-09 09:46:02 -0700

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,4 +1,44 @@
 
 public class Cat {
-
+	String breed;
+	int cuteness;
+	int intelligence;
+	int friendliness;
+	int RP;
+	public Cat(String bre, int cute, int smart, int friendly, int R){
+		breed = bre;
+		cuteness = cute;
+		friendliness = friendly;
+		RP = R;
+	}
+	/*
+	public void doesMove(FightScreen screen, ){
+	//communicates to the screen a move
+*/
+	public String getBreed(){
+		return breed;
+	}
+	public int getCuteness(){
+		return cuteness;
+	}
+	public int getIntelligence(){
+		return intelligence;
+	}
+	public int getFriendliness(){
+		return friendliness;
+	}
+	public int getRP(){
+		return RP;
+	}
+	public void getsHurt(int damage){
+		RP -= damage;
+	}
+	public void usesPotion(Potion potion){
+		int p = potion.getRP();
+		RP += p;
+	}
+	public boolean wins(Cat otherCat){
+		return (otherCat.getRP() <=0);
+	}
+	
 }
