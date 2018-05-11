2de057251879eef31adf933c2125506f5876ec99
Jessica <jess.lam948@gmail.com>
2018-05-11 08:53:07 -0700

    Update FSController.java
    Created setOtherCat method and setRP method and created the myCat instance variable

--- FSController.java
+++ FSController.java
@@ -1,14 +1,33 @@
 
 public class FSController {
 	private FSGUI myGui;
+	private Cat myCat;
 	
-	public FSController()
+	public FSController(Cat cat)
 	{
-		
+		myCat = cat;
+		Cat other = setOtherCat();
 	}
 
 	public void setGui (FSGUI gui)
 	{
 		myGui = gui;
+	}
+	
+	public Cat setOtherCat()
+	{
+		int cute = myCat.getCuteness() + (int)(Math.random() * 10);
+		int smart = myCat.getIntelligence() + (int)(Math.random() * 10);
+		int friendly = myCat.getFriendliness() + (int)(Math.random() * 10);
+		int R = myCat.getRP() + (int)(Math.random() * 10);
+		
+		Cat other = new Cat("Tabby", cute, smart, friendly, R); // need a way to randomly generate breed
+		
+		return other;
+	}
+	
+	public void setRP()
+	{
+		
 	}
 }
