599cdd3d9b7970e440de906ded5457f028e95adc
Jessica <jess.lam948@gmail.com>
2018-05-11 09:28:21 -0700

    Update FSController.java
    Set the RP in the GUI, and created attack and play methods

--- FSController.java
+++ FSController.java
@@ -2,24 +2,31 @@
 public class FSController {
 	private FSGUI myGui;
 	private Cat myCat;
+	private Cat otherCat;
+	private int myRP;
+	private int otherRP;
 	
 	public FSController(Cat cat)
 	{
 		myCat = cat;
-		Cat other = setOtherCat();
+		myRP = myCat.getRP();
+		
+		otherCat = setOtherCat();
+		otherRP = otherCat.getRP();
 	}
 
 	public void setGui (FSGUI gui)
 	{
 		myGui = gui;
+		setRP();
 	}
 	
 	public Cat setOtherCat()
 	{
-		int cute = myCat.getCuteness() + (int)(Math.random() * 10);
-		int smart = myCat.getIntelligence() + (int)(Math.random() * 10);
-		int friendly = myCat.getFriendliness() + (int)(Math.random() * 10);
-		int R = myCat.getRP() + (int)(Math.random() * 10);
+		int cute = myCat.getCuteness() + (int)(Math.random() * 5);
+		int smart = myCat.getIntelligence() + (int)(Math.random() * 5);
+		int friendly = myCat.getFriendliness() + (int)(Math.random() * 5);
+		int R = myCat.getRP() + (int)(Math.random() * 5);
 		
 		Cat other = new Cat("Tabby", cute, smart, friendly, R); // need a way to randomly generate breed
 		
@@ -27,6 +34,27 @@ public class FSController {
 	}
 	
 	public void setRP()
+	{
+		myGui.setRP(myCat, otherCat);
+	}
+	
+	public void myAttack(String move)
+	{
+		
+	}
+	
+	public void otherAttack()
+	{
+		int ran = (int)(Math.random() * 4) + 1;
+		
+		switch (ran)
+		{
+			case 1:
+				
+		}
+	}
+	
+	public void play()
 	{
 		
 	}
