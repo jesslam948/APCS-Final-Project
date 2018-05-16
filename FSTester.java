be371d96a4da588500317c524caf41569430a42b
Jessica <jess.lam948@gmail.com>
2018-05-16 10:06:19 -0700

    Update FSTester.java

--- FSTester.java
+++ FSTester.java
@@ -5,13 +5,15 @@ public class FSTester {
 
 	public static void main (String[] args)
 	{
-		Cat myCat = new Cat ("Black", 20, 20, 20, 20);
+		Cat myCat = new Cat ("Black", 20, 20, 20, 20); //when other screens are added, make this be chosen based off of what the user chooses
 		FSController fight = new FSController(myCat);
+		
 			EventQueue.invokeLater(new Runnable() {
 				public void run() {
 					try {
 						FSGUI window = new FSGUI(fight);
 						fight.setGui(window);
+						window.playerTurn();
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
