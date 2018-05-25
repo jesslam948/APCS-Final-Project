86e5fdbb5d84735dd5fc2f976b2a111af3ccc5c2
elliecho98 <36868474+elliecho98@users.noreply.github.com>
2018-05-25 11:14:50 -0700

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,3 +1,4 @@
+import javax.swing.ImageIcon;
 public class Cat {
 	private int breed;
 	private int cuteness;
@@ -15,6 +16,53 @@ public class Cat {
 	public int getBreed(){
 		return breed;
 	}
+	public String breedToString(){
+		if(breed == 0)
+			return "Mottled";
+		if(breed == 1)
+			return "Black";
+		if(breed==2)
+			return "Tabby";
+		if(breed==3)
+			return "White";
+		if(breed==4)
+			return "Siamese";
+		if(breed == 5)
+			return "Tuxedo";
+		if(breed == 6)
+			return "Calico";
+		if(breed==7){
+			return "Bengal";
+		}
+		if(breed == 8)
+			return "Russian Blue";
+		return null;
+	}
+	
+	public ImageIcon getImgIcon(){
+		if(breed == 0)
+			return new ImageIcon("H:\\MottledCat.png");
+		if(breed == 1)
+			return new ImageIcon("\\\\dohome5\\home5$\\Student5\\1848534\\blackcat.png");
+		if(breed==2)
+			return new ImageIcon("\\\\dohome5\\home5$\\Student5\\1848534\\tabbycat.png");
+		if(breed==3)
+			
+		if(breed==4)
+			
+		if(breed == 5)
+			
+		if(breed == 6)
+			
+		if(breed==7){
+			
+		}
+		if(breed == 8)
+			
+		return null;
+		return null;
+	}
+
 	public int getCuteness(){
 		return cuteness;
 	}
