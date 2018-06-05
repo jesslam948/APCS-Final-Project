3ae2d40786a6d01122fba2f8b41f6a180ecc3f8f
Jessica <jess.lam948@gmail.com>
2018-06-05 09:28:36 -0700

    Update and rename SClickListener to SClickListener.java

---
+++ SClickListener.java
@@ -0,0 +1,34 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class SClickListener implements ActionListener{
+	private final StoreGUI fscreen;
+	
+	public SClickListener(StoreGUI gui){
+		fscreen = gui;
+	}
+	
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		
+		//TODO
+//		if (e.getActionCommand().equals("Fish")){
+//			fscreen.buyFish();
+//		}
+//		else if (e.getActionCommand().equals("Salmon juice")){
+//			fscreen.buyPink();
+//		}
+//		else if (e.getActionCommand().equals("Cheese juice")){
+//			fscreen.buyOrange();
+//		}
+//		else if (e.getActionCommand().equals("Catnip juice")){
+//			fscreen.buyGreen();
+//		}
+//		fscreen.delete();
+//		
+	}
+
+}
